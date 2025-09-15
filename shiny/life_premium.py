25c70de887342075dd24e9f370ab598bce140a55
Ollie Baccay <olliebaccay@Ollies-MacBook-Pro.local>
2025-09-15 12:44:26 -0400

    Added nictone/tobacco, and default dropdown options

--- shiny/life_premium.py
+++ shiny/life_premium.py
@@ -3,18 +3,26 @@ import numpy as np
 
 
 # Life insurance premium calculation function
-def calculate_premium(age, death_benefit, bmi, sex, loading=0.2):
+def calculate_premium(age, death_benefit, bmi, sex, nicotine, loading=0.2):
     prob_death = 0.001 * (age / 30)
 
-    # adjust probability based on BMI category
     if 40 > bmi >= 30:
         prob_death *= 1.3  # higher risk if obese
     elif bmi < 18.5:
         prob_death *= 1.1  # slightly higher risk if underweight
     elif bmi >= 40:
-        prob_death *= 1.5
+        prob_death *= 1.7 # higher risk if majorly obese
+
     if sex=='Male':
-        prob_death *=1.05
+        prob_death *=1.1
+
+    if nicotine=='Cigarettes':
+        prob_death *=2
+    elif nicotine=='Occasional':
+        prob_death *=1.1
+    elif nicotine=='vaping':
+        prob_death *=1.3
+
     expected_cost = prob_death * death_benefit
     premium = expected_cost * (1 + loading)
     return round(premium, 2)
@@ -27,24 +35,29 @@ height_options = {
     72: "6'0\"", 73: "6'1\"", 74: "6'2\"", 75: "6'3\"", 76: "6'4\"", 77: "6'5\"",
     78: "6'6\"", 79: "6'7\"", 80: "6'8\"", 81: "6'9\"", 82: "6'10\"", 83: "6'11\""
 }
-
+db_options = {100000:"$100,000", 250000:"$250,000", 500000:"$500,000",
+              1000000:"$1,000,000", 2000000:"$2,000,000", 5000000:"$5,000,000",10000000:"$10,000,000"}
 app_ui = ui.page_sidebar(
     ui.sidebar(
         ui.h4("Get a term life insurance quote:"),
         ui.input_select("gender", "Sex assigned at birth:", {"Male": "Male", "Female": "Female"}),
         ui.input_slider("age", "Age:", min=18, max=75, value=30),
         ui.input_slider("weight", "Weight (lbs):", min=50, max=450, value=150, step=1),
-        ui.input_select("height", "Height", height_options),
-        ui.input_select("benefit", "Death Benefit:",
-                        {100000:"$100,000", 250000:"$250,000", 500000:"$500,000",
-                         1000000:"$1,000,000", 2000000:"$2,000,000", 5000000:"$5,000,000",10000000:"$10,000,000"})
+        ui.input_select("height", "Height", height_options, selected=67),
+        ui.input_select(
+            "nicotine", "Nicotine/tobacco usage?",
+            {"No": "No", "Cigarettes": "Regular Smoker", "Occasional": "Occasional Smoker",
+             "vaping": "Chewing Tobacco/Vaping"}
+        ),
+        ui.input_select("benefit", "Death Benefit:", db_options, selected=1000000),
     ),
     ui.div(
         ui.h4("Here’s what term life might cost"),
         ui.output_text_verbatim("premium"),
-    style="background-color: lightgrey; min-height: 10vh; padding: 250px;"
+        style="background-color: lightgrey; min-height: 10vh; padding: 250px;"
     )
 )
+
 
 # Server logic
 def server(input, output, session):
@@ -58,12 +71,12 @@ def server(input, output, session):
 
     @reactive.Calc
     def premium_value():
-        return calculate_premium(input.age(), int(input.benefit()), bmi_value(), input.gender())
+        return calculate_premium(input.age(), int(input.benefit()), bmi_value(), input.gender(), input.nicotine())
 
     @output
     @render.text
     def premium():
         return f"Estimated Monthly Premium: ${np.round(premium_value()/12,2)}\nEstimated Annual Premium: ${premium_value()}"
 
-# Creating the app
+
 app = App(app_ui, server)
